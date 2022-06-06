c6743d592d8d9537698eb00c4b82b77ded04905c
Notepad96 <notepad1029@gmail.com>
2022-06-06 23:29:47 +0900

    update code

--- example02/lib/main.dart
+++ example02/lib/main.dart
@@ -19,16 +19,25 @@ class MyAppState extends State<MyApp> {
     'pounds (lbs)': 6,
     'ounces': 7,
   };
-  final List<String> _measures = ['meters', 'kilometers', 'grams', 'kilogram', 'feet', 'miles', 'pounds(lbs)', 'ounces'];
+  final List<String> _measures = [
+    'meters',
+    'kilometers',
+    'grams',
+    'kilogram',
+    'feet',
+    'miles',
+    'pounds(lbs)',
+    'ounces'
+  ];
   final dynamic _formulas = {
-    '0':[1,0.001,0,0,3.28084,0.000621371,0,0],
-    '1':[1000,1,0,0,3280.84,0.621371,0,0],
-    '2':[0,0,1,0.0001,0,0,0.00220462,0.035274],
-    '3':[0,0,1000,1,0,0,2.20462,35.274],
-    '4':[0.3048,0.0003048,0,0,1,0.000189394,0,0],
-    '5':[1609.34, 1.60934,0,0,5280,1,0,0],
-    '6':[0,0,453.592,0.453592,0,0,1,16],
-    '7':[0,0,28.3495,0.0283495,3.28084,0,0.0625, 1],
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
   };
 
   late double _numberFrom;
@@ -137,7 +146,8 @@ class MyAppState extends State<MyApp> {
                             _numberFrom == 0) {
                           return;
                         } else {
-                          convert(_numberFrom, _startMeasure, _convertedMeasure);
+                          convert(
+                              _numberFrom, _startMeasure, _convertedMeasure);
                         }
                       },
                       child: Text(
@@ -147,7 +157,10 @@ class MyAppState extends State<MyApp> {
                   Spacer(
                     flex: 2,
                   ),
-                  Text((_resultMessage == null) ? '' : _resultMessage, style: labelStyle,),
+                  Text(
+                    (_resultMessage == null) ? '' : _resultMessage,
+                    style: labelStyle,
+                  ),
                   Spacer(
                     flex: 8,
                   ),
